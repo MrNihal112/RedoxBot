9ce3a4f99d581db1f713f90fbc283013383ec351
MrNihal112 <119762395+MrNihal112@users.noreply.github.com>
2023-03-03 05:44:24 +0530

    Update misc.py

--- Redox/modules/misc.py
+++ Redox/modules/misc.py
@@ -119,7 +119,7 @@ def markdown_help_sender(update: Update):
 @typing_action
 def src(update, _):
     update.effective_message.reply_text(
-        "Hey there! You can find what makes me click [here](https://github.com/MrNihalXd/RedoxRobot).",
+        "Hey there! You can find what makes me click [here](https://t.me/Mr_Nihal9).",
         parse_mode=ParseMode.MARKDOWN,
         disable_web_page_preview=True,
     )
